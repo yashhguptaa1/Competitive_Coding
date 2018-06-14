495a6f479bf8395869cee3548f3ff67aeca50e93
Yash Gupta <yashhguptaa1@gmail.com>
2018-06-14 15:21:33 +0530

    Update 5Numberofdigits.java

--- Unit2 Bas_Adv Recursion/5Numberofdigits.java
+++ Unit2 Bas_Adv Recursion/5Numberofdigits.java
@@ -3,14 +3,16 @@
 import java.io.*;
 
 class GFG {
-    //Top down approach
+    
     public static int count(int n){
 		//BASE CASE
 		if(n == 0){//we return 0rather than 1 cauz we would get 1 more digit than req
 			return 0;//no more digit remaining at end of recursion
 		}
 		//RECURSIVE CALL
-		int lastcount = count(n / 10);
+	        //lastcount saves the count of digits till at any moment
+	    	int enddigit=n/10;
+		int lastcount = count(enddigit);
 		return lastcount+1;//on not using +1, lastcount would not increment and lastcount
                             //as from base case would return
     }
